eea7e839db2acadade2d45be755d010c29d59894
janniks <janniks@users.noreply.github.com>
2024-10-17 15:58:59 +0200

    chore: update eslint settings

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,11 +1,12 @@
 // @ts-check
 import eslint from '@eslint/js';
 import tseslint from 'typescript-eslint';
+import prettier from 'eslint-plugin-prettier/recommended';
 
 export default tseslint.config(
   eslint.configs.recommended,
   ...tseslint.configs.recommended,
-
+  prettier,
   // jest file config
   {
     files: ['**/jest.*.{js,mjs}'],
